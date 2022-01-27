07d0c347870443853e7f2fb08155394ba4ef4d4e
Thomas Qian <thomasqian@google.com>
2022-01-27 23:43:50 +0800

    First Try

---
+++ build/precache-manifest.634278445b48b8eb7b83c5bda898bec4.js
@@ -0,0 +1,42 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "f9cb4bc19f040a16d87aa6d169f6a6c4",
+    "url": "/index.html"
+  },
+  {
+    "revision": "a07aa39384509c40c06f",
+    "url": "/static/css/2.18fb2ca0.chunk.css"
+  },
+  {
+    "revision": "4a8ada31cd5d9ae24db1",
+    "url": "/static/css/main.1d20ac0f.chunk.css"
+  },
+  {
+    "revision": "a07aa39384509c40c06f",
+    "url": "/static/js/2.3b078bbb.chunk.js"
+  },
+  {
+    "revision": "ef38c0a84f87ea964f81021e97df7d09",
+    "url": "/static/js/2.3b078bbb.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "4a8ada31cd5d9ae24db1",
+    "url": "/static/js/main.70801738.chunk.js"
+  },
+  {
+    "revision": "77dc8c1b5387ecfd3c2d",
+    "url": "/static/js/runtime-main.f9dcea19.js"
+  },
+  {
+    "revision": "14c8a27aebe5c88df050bb72d76bdbe9",
+    "url": "/static/media/dataGovernor.14c8a27a.svg"
+  },
+  {
+    "revision": "72d9c29fea54f0ac2ee7c172fa996451",
+    "url": "/static/media/dataUser.72d9c29f.svg"
+  },
+  {
+    "revision": "b0b6331b0e5ea4e2534e4bf80ad3d67d",
+    "url": "/static/media/itAdmin.b0b6331b.svg"
+  }
+]);
